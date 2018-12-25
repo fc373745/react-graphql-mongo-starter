7db1e4dd47c47baf324cbcece827a54696d28799
francis <c4248@outlook.com>
2018-12-25 12:06:28 -0800

    part 3 - Schemas and Resolvers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql')
 
 const app = express();
 
@@ -8,5 +10,32 @@ app.use(bodyParser.json())
 app.get('/', (req, res, next) => {
     res.send('hello')
 })
+
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic', 'sailing', 'coding']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}))
 
 app.listen(3000)
